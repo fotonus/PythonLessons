55b85ea0fd1402613f2dfa01e75956ad4909381f
Vladimir <romanovka@gmail.com>
2020-01-13 23:24:01 +0300

    Домашка 06 сделана

---
+++ lesson_002/05_zoo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# есть список животных в зоопарке
+
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+
+# посадите медведя (bear) между львом и кенгуру
+#  и выведите список на консоль
+zoo.insert(1,'bear')
+print(zoo)
+
+# добавьте птиц из списка birds в последние клетки зоопарка
+birds = ['rooster', 'ostrich', 'lark', ]
+#  и выведите список на консоль
+zoo.extend(birds)
+print(zoo)
+
+# уберите слона
+#  и выведите список на консоль
+zoo.pop(3)
+print(zoo)
+
+# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
+# Номера при выводе должны быть понятны простому человеку, не программисту.
+cell_lion = zoo.index('lion')+1
+cell_lark = zoo.index('lark')+1
+print('В клетке номер', cell_lion, 'сидит', zoo[0], ', а под номером', cell_lark, 'сидит', zoo[6])
+
+
